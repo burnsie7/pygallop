287b4b1bcec23ba7d4e39d27a2a4cac7690062b5
bburns-ddog <benjamin.burns@datadoghq.com>
2017-08-24 23:51:06 -0700

    nada

--- tp.py
+++ tp.py
@@ -4,18 +4,35 @@ import random
 
 line_chart = pygal.Line()
 line_chart.title = 'lines'
-line_chart.x_labels = map(str, range(0, 400))
+days = 7
+line_chart.x_labels = map(str, range(0, 360*days))
 y_vals = []
 y_range = 100
 thing = 360
-for x in range(0, thing):
-    y = math.sin(math.radians(x)) * y_range + y_range
-    if y < y_range:
-        y = y // 4
-    rando = random.random() + 2
-    y = math.floor(y*rando) // 2 + y_range + math.floor(rando*100) // 2 + y_range
-    if y < thing:
-        y = y + 100
-    y_vals.append(y)
+for nmb in range(days):
+    start = nmb*thing
+    end = nmb*thing + thing
+    start_rand = random.randint(2,5)
+    end_rand = random.randint(2,5)
+    up_slope = random.randint(1,20)
+    dn_slope = random.randint(1,20)
+    for x in range(start, end):
+        half = (end + start) // 2
+        fact = 1
+        if x < half:
+            fact = random.random() * 3
+        else:
+            fact = random.random() * 3
+        y = math.sin(math.radians(x)) * fact
+        if nmb == 3 or nmb == 4:
+            y = y / 2
+        else:
+            y = y * 2
+        if y < 0:
+            y = y / 2
+        # randoms
+        y = (y + 10) * 100
+        # change slope
+        y_vals.append(y)
 line_chart.add('line', y_vals)
 line_chart.render_to_png('snoos.png')
